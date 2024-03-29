80c5f6ca7771958e2650e608ac0cc2ab41195861
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-03-29 20:56:24 +0530

    Create an input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import '../styles/Input.css';
+
+export default function Input({id, value, handleChange, label}) {
+    return (
+        <section className='input-field'>
+            <label htmlFor={id}>{label}</label>
+            <input
+                type='text'
+                value={value ?? 'name'}
+                onChange={handleChange ?? null}
+            />
+        </section>
+    )
+}
